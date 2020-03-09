4529860860aad6b5de7221a650f07c65255e6021
rieull <rayoung9312@gmail.com>
2020-03-09 17:59:17 +0900

    a

--- notice-en/src/Component/TableBody.js
+++ notice-en/src/Component/TableBody.js
@@ -3,11 +3,14 @@ import './TableRow.css';
 
 export class TableBody extends React.Component{
 
-
-    render(){
+    render(){ 
+        let str=this.props.content.split('<br/>');
+                let con = str.map((s)=>{
+                    return <span>{s}<br/></span>;
+                });
         return(
             <tr className={"row-body " && this.props.className}>
-                <td colSpan="2" className="row-content ">{this.props.content}</td>
+                <td colSpan="2" className="row-content ">{con}</td>
             </tr>
         )
     }
